4abc94b6cc8ffa74fd554a60037f7f4593dfadb0
”Brisoul”~ <svelimovich@mail.ru>
2022-03-01 22:01:27 +0300

    Change java modules to maven modules, create dependencies

---
+++ console/src/main/java/console/consoleTest.java
@@ -0,0 +1,13 @@
+package console;
+
+import dao.daoTest;
+import entities.entitiesTest;
+
+public class consoleTest {
+
+    public static void main(String[] args){
+        daoTest.testView();
+        entitiesTest.testView();
+
+    }
+}
---
+++ entities/src/main/java/entities/entitiesTest.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class entitiesTest {
+    public static void testView() {
+        System.out.println("Entities invades");
+    }
+
+    public static void testViewSecond(){
+        System.out.println("Entities invades from dao");
+    }
+}

d3fd376d3bba3605f7c576d57f72092b2db60212
Brisoul <svelimovich@mail.ru>
2022-03-27 18:48:34 +0300

    remove useless libs from test

--- DAO/src/test/java/PersonalCardCRUDTest.java
+++ DAO/src/test/java/PersonalCardCRUDTest.java
@@ -1,7 +1,6 @@
 import com.netcracker.hb.Dao.CRUD.Person.IGuestCRUD;
 import com.netcracker.hb.Dao.CRUD.Person.PersonalCardCRUD;
 import com.netcracker.hb.entities.persons.PersonalCard;
-import java.util.UUID;
 import org.junit.Test;
 
 public class PersonalCardCRUDTest {
